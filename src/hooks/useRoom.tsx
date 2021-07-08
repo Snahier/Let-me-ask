071d2ae13e4ad11d99e8ba267a99d52828c82f40
Snahier <hansalmeida@hotmail.com>
2021-07-08 16:37:41 -0300

    Add like properties

--- src/hooks/useRoom.tsx
+++ src/hooks/useRoom.tsx
@@ -1,16 +1,6 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../contexts/AuthContext"
 import { database } from "../services/firebase"
-
-type IQuestion = {
-  id: string
-  author: {
-    name: string
-    avatar: string
-  }
-  content: string
-  isAnswered: boolean
-  isHighlighted: boolean
-}
 
 type FirebaseQuestions = Record<
   string,
@@ -22,12 +12,33 @@ type FirebaseQuestions = Record<
     content: string
     isAnswered: boolean
     isHighlighted: boolean
+    likes: Record<
+      string,
+      {
+        authorId: string
+      }
+    >
   }
 >
 
+type IQuestion = {
+  id: string
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isAnswered: boolean
+  isHighlighted: boolean
+  likeCount: number
+  likeId: string | undefined
+}
+
 export const useRoom = (roomId: string) => {
+  const { user } = useContext(AuthContext)
   const [title, setTitle] = useState()
   const [questions, setQuestions] = useState<IQuestion[]>([])
+  const [authorId, setAuthorId] = useState()
 
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`)
@@ -43,13 +54,20 @@ export const useRoom = (roomId: string) => {
           author: value.author,
           isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(
+            ([key, like]) => like.authorId === user?.id
+          )?.[0],
         })
       )
 
       setTitle(databaseRoom.title)
       setQuestions(parsedQuestions)
+      setAuthorId(databaseRoom.authorId)
     })
-  }, [roomId])
 
-  return { questions, title }
+    return () => roomRef.off("value")
+  }, [roomId, user?.id])
+
+  return { questions, title, authorId }
 }
